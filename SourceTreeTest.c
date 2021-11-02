5df25f38b875a96be33c66153270507108c6d96b
SHyeMin <daneil2005@naver.com>
2021-11-02 17:17:43 +0900

    Test 브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,8 +2,14 @@
 
 int main()
 {
-  printf("First OSS Code\n");
-  printf("Third OSS Code\n");
+  int i=0;
 
+  printf("First OSS Code\n");
+  printf("Second OSS Code\n");
+
+  for (int i=0;i<10;i++)
+  {
+    printf("test 브랜치\n");
+  }
   return 0;
 }
